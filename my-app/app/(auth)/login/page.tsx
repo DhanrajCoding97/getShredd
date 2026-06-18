e03737f5b4ae73f7fe5f1306ce6249f8898a8bcf
DhanrajCoding97 <dhanrajcoding@gmail.com>
2026-06-18 22:31:06 +0530

    added react-hook-form and login server action

--- my-app/app/(auth)/login/page.tsx
+++ my-app/app/(auth)/login/page.tsx
@@ -4,7 +4,6 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod"
 import { Controller, useForm } from "react-hook-form"
 import { toast } from "sonner"
-
 import { Button } from "@/components/ui/button"
 import {
   Field,
@@ -15,9 +14,8 @@ import {
 } from "@/components/ui/field"
 import { Input } from "@/components/ui/input"
 import ShaderBackground from '@/components/ShaderBackground';
-
-
-
+import { login } from '@/app/actions/login';
+import { FcGoogle } from "react-icons/fc";
 
 export default function LoginPage() {
   const supabase = createClient()
@@ -54,17 +52,82 @@ export default function LoginPage() {
     }
   })
 
+  //onSubmit function
+  // async function onSubmit(data: z.infer<typeof formSchema>) {
+  //   const res = await login
+  //   if (res.success) {
+  //     form.reset()
+  //     toast.success("logged in successfully", {
+  //       description: JSON.stringify(data,null,2),
+  //       className:"whitespace-pre-wrap font-mono"
+  //     })
+  //   } else {
+  //     toast.error("Failed to log in")
+  //   }
+  // }
+
+  async function onSubmit(data: z.infer<typeof formSchema>) {
+  const res = await login(data);
+
+  if (res.success) {
+    form.reset();
+    toast.success("Logged in successfully");
+  } else {
+    toast.error(res.error ?? "Failed to log in");
+  }
+}
+
   return (
     <>
-    <ShaderBackground/>
-    <main className='min-h-screen grid place-content-center'>
-      <div className='px-6 py-4 h-full w-full bg-white-700 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100
-'>
+    {/* <ShaderBackground/> */}
+    <main className='min-h-screen grid place-content-center bg-black'>
+      <div className='px-6 py-4 h-full w-full rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-0 border border-gray-100'>
         <div className='flex flex-col gap-1 items-start justify-start w-full'>
           <h1 className='text-2xl font-bold text-white'>Login to your account </h1>
           <p className='text-[#A1A1A1]'>Enter your email below to login to your account</p>
-          </div>
-        <button onClick={signInWithGoogle}>Sign in with Google</button>
+        </div>
+        
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FieldGroup>
+              <Controller
+                name='email'
+                control={form.control}
+                render={({field, fieldState}) => (
+                  <Field data-invalid={fieldState.invalid}>
+                    <FieldLabel htmlFor={field.name}>Email</FieldLabel>
+                    <Input 
+                      {...field}
+                      id={field.name}
+                      aria-invalid={fieldState.invalid}
+                    />
+                    {fieldState.invalid && (
+                      <FieldError errors= {[fieldState.error]}/>
+                    )}
+                  </Field>
+                )}
+              />
+              <Controller
+                name='password'
+                control={form.control}
+                render={({field, fieldState}) => (
+                  <Field data-invalid={fieldState.invalid}>
+                    <FieldLabel htmlFor={field.name}>Password</FieldLabel>
+                    <Input 
+                      {...field}
+                      id={field.name}
+                      aria-invalid={fieldState.invalid}
+                    />
+                    {fieldState.invalid && (
+                      <FieldError errors= {[fieldState.error]}/>
+                    )}
+                  </Field>
+                )}
+              />
+            <Button variant="outline" className='w-full'>Log in</Button>
+            </FieldGroup>
+        </form>
+            <Button variant="default" onClick={signInWithGoogle}>Sign in with Google <FcGoogle/> </Button>
+      
       </div>
     </main>
     </>
---
+++ my-app/app/actions/login.ts
@@ -0,0 +1,25 @@
+'use server';
+
+import { createClient } from "@/lib/supabase/server";
+
+export async function login (data: {
+    email:string,
+    password: string
+}) {
+    const supabase = await createClient();
+
+    const {error} = await supabase.auth.signInWithPassword({
+        email: data.email,
+        password: data.password,
+    })
+
+    if(error) {
+        return {
+            success: false,
+            error: error.message,
+        }
+    }
+    return {
+        success: true,
+    }
+}
