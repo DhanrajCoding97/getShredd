726329516840ca3dabfa085ca555f3b879017132
DhanrajCoding97 <dhanrajcoding@gmail.com>
2026-07-01 15:07:11 +0530

    updated meal card icons

--- my-app/components/MealCard.tsx
+++ my-app/components/MealCard.tsx
@@ -1,8 +1,8 @@
 import { BsThreeDotsVertical } from 'react-icons/bs';
 import { Button } from './ui/button';
 import Image from 'next/image';
-import { ProteinFood } from './svgs';
-
+import { ProteinFood, CarbFood, FatFood } from './svgs';
+// import { CarbFood } from './svgs';
 export type MealCardProps = {
     name: string;
     calories: number;
@@ -23,7 +23,7 @@ export default function MealCard({
     mealType,
 }: MealCardProps) {
     return (
-        <div className='flex h-34 w-fit transform flex-col rounded-xl border border-gray-100 bg-[#0d9a5f] p-4 shadow-md transition-all duration-300 ease-linear hover:-translate-y-2 hover:shadow-2xl dark:bg-[#0d9a5f]'>
+        <div className='flex max-w-fit transform flex-col rounded-xl border border-gray-100 p-2 text-[#efe1e0] shadow-md transition-all duration-300 ease-linear hover:-translate-y-2 hover:shadow-2xl dark:bg-[#091516]'>
             <div className='flex items-start justify-between gap-4'>
                 <div className='flex flex-col'>
                     <span className='text-lg leading-normal font-bold'>
@@ -42,24 +42,27 @@ export default function MealCard({
                     <BsThreeDotsVertical />
                 </Button>
             </div>
-            <div className='flex items-center gap-2'>
-                <div className='flex flex-col gap-1'>
+            <div className='flex flex-col justify-center gap-2'>
+                <div className='flex items-center gap-1'>
                     {/* <span className='text-muted text-sm font-light'>
                         Protein
                     </span> */}
-                    <ProteinFood className='h-10 w-10' />
+                    <ProteinFood className='h-7 w-7' />
                     <span className='text-lg leading-tight font-semibold'>
                         {proteinG}g
                     </span>
                 </div>
-                <div className='flex flex-col gap-1'>
-                    <span className='text-muted text-sm font-light'>Carbs</span>
+                <div className='flex items-center gap-1'>
+                    {/* <span className='text-muted text-sm font-light'>Carbs</span> */}
+
+                    <CarbFood className='h-7 w-7' />
                     <span className='text-lg leading-tight font-semibold'>
                         {carbsG}g
                     </span>
                 </div>
-                <div className='flex flex-col gap-1'>
-                    <span className='text-muted text-sm font-light'>Fat</span>
+                <div className='flex items-center gap-1'>
+                    {/* <span className='text-muted text-sm font-light'>Fat</span> */}
+                    <FatFood className='h-7 w-7' />
                     <span className='text-lg leading-tight font-semibold'>
                         {fatG}g
                     </span>
--- my-app/components/svgs/index.ts
+++ my-app/components/svgs/index.ts
@@ -1,1 +1,3 @@
-export { default as ProteinFood } from './proteinFood.svg';
+export { default as ProteinFood } from './protein-food.svg';
+export { default as CarbFood } from './carb-food.svg';
+export { default as FatFood } from './fat-food.svg';
